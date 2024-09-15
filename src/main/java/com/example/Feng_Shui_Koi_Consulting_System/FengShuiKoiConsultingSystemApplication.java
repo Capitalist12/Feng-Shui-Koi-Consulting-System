82b6f2ac7d3117504eb08ae04c989a8891d0c1e9
dothang <dt2005.26@gmail.com>
2024-09-15 21:32:48 +0700

    Start

---
+++ src/main/java/com/example/Feng_Shui_Koi_Consulting_System/FengShuiKoiConsultingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Feng_Shui_Koi_Consulting_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FengShuiKoiConsultingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FengShuiKoiConsultingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Feng_Shui_Koi_Consulting_System/FengShuiKoiConsultingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Feng_Shui_Koi_Consulting_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FengShuiKoiConsultingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
