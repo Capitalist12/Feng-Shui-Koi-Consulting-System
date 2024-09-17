82035826aad6a94fd10b95852062f0d24da1bbb1
DESKTOP-1M9SI46\612t1 <dinhthse183727@fpt.edu.vn>
2024-09-17 13:22:39 +0700

    17/09

---
+++ src/main/java/com/example/Feng_Shui_Koi_Consulting_System/Entity/KoiFish.java
@@ -0,0 +1,28 @@
+package com.example.Feng_Shui_Koi_Consulting_System.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class KoiFish {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+    String name;
+    String color;
+    float size;
+    float weight;
+    String elementId;
+    String typeId;
+    String description;
+    String gender;
+}
---
+++ src/main/java/com/example/Feng_Shui_Koi_Consulting_System/Entity/Tank.java
@@ -0,0 +1,24 @@
+package com.example.Feng_Shui_Koi_Consulting_System.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Tank {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+    String shape;
+    String position;
+    String directionId;
+    String elementId;
+}
