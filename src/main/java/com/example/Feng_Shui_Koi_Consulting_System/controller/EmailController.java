222bc6c19cb08e2eec9f8e40375525a0d8be1d44
DESKTOP-1M9SI46\612t1 <dinhthse183727@fpt.edu.vn>
2024-09-26 16:37:35 +0700

    Forgot Password

--- src/main/java/com/example/Feng_Shui_Koi_Consulting_System/controller/EmailController.java
+++ src/main/java/com/example/Feng_Shui_Koi_Consulting_System/controller/EmailController.java
@@ -2,19 +2,19 @@ package com.example.Feng_Shui_Koi_Consulting_System.controller;
 
 import com.example.Feng_Shui_Koi_Consulting_System.service.SendEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EmailController {
     @Autowired
     private SendEmailService sendEmailService;
 
-    @GetMapping("/sendEmail")
-    public String sendEmail(){
-        sendEmailService.sendEmail("tranhoangdinhk12adv@gmail.com", "Dm", "TestSubject");
+    @PostMapping("/sendEmail/{recipient}")
+    public String sendEmail(@PathVariable String recipient,
+                            @RequestParam String body,
+                            @RequestParam String subject){
+
+        sendEmailService.sendEmail(recipient, body, subject);
         return "Sent successfully";
     }
-
-
 }
