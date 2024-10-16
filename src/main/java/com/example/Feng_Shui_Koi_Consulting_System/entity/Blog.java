db3724a9b12afedb936b51414b3f75e137f731e0
nguyenreal <ntnn51@gmail.com>
2024-10-17 00:15:24 +0700

    Blog, Comment entity relationship

--- src/main/java/com/example/Feng_Shui_Koi_Consulting_System/entity/Blog.java
+++ src/main/java/com/example/Feng_Shui_Koi_Consulting_System/entity/Blog.java
@@ -1,4 +1,44 @@
 package com.example.Feng_Shui_Koi_Consulting_System.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Table(name = "Blog")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Blog {
+    @Id
+    @Column(name = "BlogID")
+    String blogID;
+    @Column(name = "Title")
+    String title;
+    @Column(name = "ImageID")
+    String imageID;
+    @Column(name = "Description")
+    String description;
+    @Column(name = "CreatedDate")
+    LocalDate createdDate;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "UserID", nullable = false, referencedColumnName = "UserID")
+    @JsonBackReference
+    User user;
+
+    @OneToMany(mappedBy = "blog", cascade = CascadeType.ALL
+            ,orphanRemoval = true)
+    @JsonManagedReference
+    Set<Comment> comments;
+
+    @Column(name = "BlogImageID")
+    String blogImageID;
 }
--- src/main/java/com/example/Feng_Shui_Koi_Consulting_System/entity/Comment.java
+++ src/main/java/com/example/Feng_Shui_Koi_Consulting_System/entity/Comment.java
@@ -1,4 +1,36 @@
 package com.example.Feng_Shui_Koi_Consulting_System.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Comment")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Comment {
+    @Id
+    @Column(name = "CommentID")
+    Integer commentID;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "UserID", nullable = false, referencedColumnName = "UserID")
+    @JsonBackReference
+    User user;
+
+    @Column(name = "CommentDate")
+    LocalDate commentDate;
+    @Column(name = "Content")
+    String content;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "BlogID", nullable = false, referencedColumnName = "BlogID")
+    @JsonBackReference
+    Blog blog;
 }
