8c896a39d556778bd3a8bd5209139365cafafada
DESKTOP-1M9SI46\612t1 <dinhthse183727@fpt.edu.vn>
2024-10-22 20:15:33 +0700

    add reset-password

--- src/main/java/com/example/Feng_Shui_Koi_Consulting_System/configuration/SecurityConfig.java
+++ src/main/java/com/example/Feng_Shui_Koi_Consulting_System/configuration/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
 
 
     private final String[] PUBLIC_ENDPOINT = {"/auth/login","/auth/signup","/auth/introspect"
-    ,"/auth/outbound/authentication", "/auth/verify-email", "/ad/verified"};
+    ,"/auth/outbound/authentication", "/auth/verify-email", "/ad/verified", "/auth/reset-password"};
     private final String[] SWAGGER = {
             "/koifish-docs/v3/api-docs/**",
             "/swagger-ui/**",
