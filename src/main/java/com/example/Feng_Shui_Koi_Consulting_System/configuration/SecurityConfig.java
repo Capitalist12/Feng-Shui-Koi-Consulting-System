87efa8daf3cdaed2ace6ab99f3bb6000349b1418
nguyenreal <ntnn51@gmail.com>
2024-10-28 15:25:28 +0700

    Modify

--- src/main/java/com/example/Feng_Shui_Koi_Consulting_System/configuration/SecurityConfig.java
+++ src/main/java/com/example/Feng_Shui_Koi_Consulting_System/configuration/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
 
     private final String[] PUBLIC_ENDPOINT = {"/auth/login","/auth/signup","/auth/introspect"
     ,"/auth/outbound/authentication", "/auth/verify-email", "/auth/logout"
-    , "/ad/verified", "/auth/reset-password"};
+    , "/auth/reset-password", "/ad", "/blog"};
     private final String[] SWAGGER = {
             "/koifish-docs/v3/api-docs/**",
             "/swagger-ui/**",
