4be297d7895066f944c0729896d096301d6e8cb4
nguyenreal <ntnn51@gmail.com>
2024-11-05 02:50:36 +0700

    Revert "fix reload 3"
    This reverts commit 7e494fe065662cd9e6cc09e8b6c1173d22a112a3.

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    historyApiFallback: true,
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-  },
 })
