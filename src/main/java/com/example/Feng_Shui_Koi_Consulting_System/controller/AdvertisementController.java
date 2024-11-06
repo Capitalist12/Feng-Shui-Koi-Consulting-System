f988bdf92047a8ddd06b8891f2f8c33e91fb1871
DinhTran1706 <dinht1706@gmail.com>
2024-11-06 11:44:28 +0700

    Pre-authorized

--- src/main/java/com/example/Feng_Shui_Koi_Consulting_System/controller/AdvertisementController.java
+++ src/main/java/com/example/Feng_Shui_Koi_Consulting_System/controller/AdvertisementController.java
@@ -26,6 +26,7 @@ public class AdvertisementController {
     }
 
     //Get all ads doesn't care about status
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     ApiResponse<List<AdvertisementResponse>> getAllAds(){
         return ApiResponse.<List<AdvertisementResponse>>builder()
@@ -41,7 +42,8 @@ public class AdvertisementController {
                 .build();
     }
 
-    //Get member's ads
+    //Get my ads
+    @PreAuthorize("hasAnyRole('USER', 'MEMBER', 'ADMIN')")
     @GetMapping("/get-my-ads")
     ApiResponse<List<AdvertisementResponse>> getAdsOfUser(){
         return  ApiResponse.<List<AdvertisementResponse>>builder()
